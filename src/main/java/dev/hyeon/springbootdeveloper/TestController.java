259be7a337e1ac7415aca76b46afbaf00a4868ab
SolLa <hyeonhaeju@gmail.com>
2026-09-03 16:27:07 +0900

    다양한 요청 메소드 실습
    - 다양한 요청 method

--- src/main/java/dev/hyeon/springbootdeveloper/TestController.java
+++ src/main/java/dev/hyeon/springbootdeveloper/TestController.java
@@ -1,11 +1,26 @@
 package dev.hyeon.springbootdeveloper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
     @GetMapping("/hi")
     public String hi() {
         return "안녕하세요! 에 대한 응답 리턴입니다.";
+    }
+    @GetMapping("/test")
+    public String test() {
+        return "안녕하세요! 에 대한 테스트 응답입니다.";
+    }
+    @PostMapping("/test")
+    public String posttest() {
+        return "안녕하세요! post&test에 대한 응답입니다.";
+    }
+    @PutMapping("/test")
+    public String puttest() {
+        return "안녕하세요! put&test에 대한 응답입니다.";
+    }
+    @DeleteMapping("/test")
+    public String deleteTest() {
+        return "안녕하세요! delete&test에 대한 응답입니다.";
     }
 }
